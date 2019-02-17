9560505986ae1b020954cb2e5c79feca00112dc0
Mattia Achilli <mattiaachilli9898@gmail.com>
2019-02-17 22:00:56 +0100

    Add code in controller and method in Model interface

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -3,15 +3,21 @@ package controller;
 /**
  * 
  * @author mattia
- *
+ *	Controller of the application, interacts with model and view.
  */
 
 public interface Controller {
+	
+	/**
+	 * Initialize game model
+	 */
+	void initGame();
 	
 	/*
 	 * Start game loop.
 	 */
 	void startGame();
+	
 	/**
 	 * Stop the game.
 	 */
--- src/controller/GameInit.java
+++ src/controller/GameInit.java
@@ -1,6 +1,21 @@
 package controller;
 
+/**
+ * 
+ * 
+ * @author mattia
+ * Provides methods to initialize the game.
+ *	
+ */
+
 public class GameInit {
+	/**
+     * 
+     * The main of the application.
+     * 
+     * @param args
+     *            arguments.
+     */
 	public static void main(String[] args) {
 		
 	}
