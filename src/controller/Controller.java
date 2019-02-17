3005ce62dedbdc98667c86b0873285bc6274a834
Mattia Achilli <mattiaachilli9898@gmail.com>
2019-02-17 21:29:59 +0100

    Add Controller, Model, View package and Controller Interface

---
+++ src/controller/Controller.java
@@ -0,0 +1,19 @@
+package controller;
+
+/**
+ * 
+ * @author mattia
+ *
+ */
+
+public interface Controller {
+	
+	/*
+	 * Start game loop.
+	 */
+	void startGame();
+	/**
+	 * Stop the game.
+	 */
+	void stopGame();
+}
---
+++ src/controller/GameInit.java
@@ -0,0 +1,7 @@
+package controller;
+
+public class GameInit {
+	public static void main(String[] args) {
+		
+	}
+}
