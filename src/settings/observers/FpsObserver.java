365cb1ed23bd5d88c94feb23452408a5021bd4d3
Yuri Bernardini <yuri.bernardini@studio.unibo.it>
2019-03-09 21:12:30 +0100

    implements Settings and ObserverSettings

---
+++ src/settings/observers/FpsObserver.java
@@ -0,0 +1,32 @@
+package settings.observers;
+
+import javafx.scene.control.ComboBox;
+import settings.SettingsImpl;
+
+/**
+ * 
+ * Implements FPS settings changes
+ *
+ */
+public class FpsObserver implements Observer {
+
+    private final ComboBox<Integer> fpsComboBox;
+    
+    /**
+     * 
+     * @param fpsComboBox
+     *          comboBox list of game rate
+     */
+    public FpsObserver(final ComboBox<Integer> fpsComboBox) {
+        this.fpsComboBox = fpsComboBox;
+        this.fpsComboBox.getItems().addAll(SettingsImpl.getSettings().getSupportedFPS());
+        this.fpsComboBox.setValue(SettingsImpl.getSettings().getSelectedFPS());
+    }
+    
+    @Override
+    public void update() {
+        SettingsImpl.getSettings().setSelectedFPS(this.fpsComboBox.getValue());
+
+    }
+
+}
