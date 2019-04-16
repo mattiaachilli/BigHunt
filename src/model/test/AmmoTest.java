fc4cb59ebf01799307ac3398c8bf51b5bc1aa5c3
Giulio Dulja <giuliodulja@gmail.com>
2019-04-16 11:28:38 +0200

    fixed ammo test class

--- src/model/test/AmmoTest.java
+++ src/model/test/AmmoTest.java
@@ -1,44 +1,43 @@
 package model.test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
 import model.Model;
 import model.ModelImpl;
+import model.matches.GameMode;
 
 class AmmoTest {
 
+    private static final int MAX_BULLETS = 8;
     private static final int MAX_MAGAZINES = 20;
     private final Model model = new ModelImpl();
 
     @Test
     void test() {
-        this.model.shoot();
-        assertEquals(this.model.getCurrentMagazine().getNumber(), 1);
-        assertEquals(this.model.getBullets(), 4);
-        for (int i = 0; i <=  MAX_MAGAZINES / 4; i++) {
+        this.model.initGame(GameMode.STORY_MODE); //Initializing story mode
+        this.model.shoot(); //Shoots one time
+        assertEquals(this.model.getCurrentMagazine().getNumber(), 1); //Should still be at the first magazine
+        assertEquals(this.model.getBullets(), MAX_BULLETS - 1); //Should have seven bullets
+        for (int i = 0; i <=  MAX_MAGAZINES / 2; i++) {
+            if (!this.model.canShoot()) {
+                this.model.recharge(); //Simulating a recharge when needed
+            }
             this.model.shoot();
         }
-        assertEquals(this.model.getCurrentMagazine().getNumber(), 2);
-        assertEquals(this.model.getBullets(), 3);
-//        this.model.activateInfAmmo();
-//        for (int i = 0; i <= ModelImpl.MAX_MAGAZINES; i++) {
-//            this.model.shoot();
-//        }
-//        assertEquals(this.model.getCurrentMagazine().getNumber(), 2);
-//        assertEquals(this.model.getBullets(), 3);
-//        this.model.deactivateInfAmmo();
-        for (int i = 0; i <= MAX_MAGAZINES / 4; i++) {
-            this.model.shoot();
+        assertEquals(this.model.getCurrentMagazine().getNumber(), 2); //Should be using the second magazine
+        assertEquals(this.model.getBullets(), MAX_BULLETS - 4); //With five bullets remaining
+        for (int i = 0; i <= MAX_MAGAZINES / 2; i++) {
+            this.model.shoot(); //Testing without recharging
         }
-        assertEquals(this.model.getCurrentMagazine().getNumber(), 3);
-        assertEquals(this.model.getBullets(), 2);
-//        for (int i = 0; i < ModelImpl.MAX_MAGAZINES * 5; i++) {
-//            this.model.shoot();
-//        }
-//        assertEquals(this.model.getCurrentMagazine(), ModelImpl.MAX_MAGAZINES);
-//        assertEquals(this.model.getBullets(), 0);
+        assertEquals(this.model.getCurrentMagazine().getNumber(), 2); //Should still be using the second magazine but with no ammo left
+        assertEquals(this.model.getBullets(), 0);
+        for (int i = 0; i <= MAX_MAGAZINES; i++) {
+            this.model.recharge(); //Using every  magazine
+        }
+        assertTrue(this.model.isGameOver()); //Should be game over for story mode rules
     }
 
 }
