e02b1ecce5645cb8968887a2a8107de812f962c1
Mattia Achilli <mattia.achilli@studio.unibo.it>
2019-04-23 14:42:24 +0200

    Delete a Javadoc comment

--- src/model/spawner/duck/AbstractSpawner.java
+++ src/model/spawner/duck/AbstractSpawner.java
@@ -9,7 +9,7 @@ import model.entities.Duck;
 
 /**
  * 
- * AbstractSpwanwer: Survival and story. I was inspired by the MagnumChaos project for this implementation.
+ * AbstractSpwanwer: Survival and story.
  *
  */
 
